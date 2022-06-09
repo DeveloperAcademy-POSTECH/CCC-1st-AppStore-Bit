72b845a35603e0c43b5dc1064ba03994de99a12e
yeongwoo_Cho <jyy0223@naver.com>
2022-06-09 12:16:49 +0900

    [Add]: profileButton(#3)

--- AppStore/AppViewController.swift
+++ AppStore/AppViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class AppViewController: UIViewController {
 
+    lazy var profileButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "person.crop.circle"), for: .normal)
+        
+        let pointSize: CGFloat = 30
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: pointSize)
+        var config = UIButton.Configuration.plain()
+        if #available(iOS 15.0, *) {
+            config.preferredSymbolConfigurationForImage = imageConfig
+            button.configuration = config
+        }
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureView()
         configureNavigationBar()
+        configureSubViews()
+        configureConstraints()
     }
     
     private func configureView() {
@@ -27,5 +43,22 @@ class AppViewController: UIViewController {
         
         navController.navigationBar.prefersLargeTitles = true
         self.navigationItem.title = TabBarPage.app.pageTitleValue()
+    }
+    
+    private func configureSubViews() {
+        guard let view = self.view else { return }
+        
+        view.addSubview(profileButton)
+    }
+    
+    private func configureConstraints() {
+        guard let view = self.view else { return }
+        
+        profileButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraints([
+            profileButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            profileButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                                    constant: -10),
+        ])
     }
 }
